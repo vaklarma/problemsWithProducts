bb10f24e68ac74f3682ab94d5ffbf8829f03a288
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2019-01-26 22:17:14 +0100

    (add) Font awsome icons integrated in assets folder (modify) Landing page with login form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './user/login/login.component';
+import { LoginFormComponent } from './user/login-form/login-form.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { JumbotronComponent } from './core/jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent,
+    NavbarComponent,
+    HomeComponent,
+    JumbotronComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
